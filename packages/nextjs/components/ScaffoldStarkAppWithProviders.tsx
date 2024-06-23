555337d1cc7ab19532d8dea5aabb98e1c7971c32
metalboyrick <rsulisthio@gmail.com>
2024-06-23 13:11:46 +0700

    feat: remove scaffold headers and footers

--- packages/nextjs/components/ScaffoldStarkAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldStarkAppWithProviders.tsx
@@ -49,9 +49,7 @@ const ScaffoldStarkApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <Header />
         <main className="relative flex flex-col flex-1">{children}</main>
-        <Footer />
       </div>
       <Toaster />
     </>
